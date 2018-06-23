4c0d836ec79b42dca9ed88e00281b54d9a20f296
Anshuman Bora <anshumanbora04@gmail.com>
2018-06-23 11:21:58 -0700

    repo initialized with starting directories + bootstrap

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Footer from './components/Footer';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+
+          <Footer/>
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+
+
+
+export default class Footer extends Component{
+
+    render(){
+        return(
+            <div className="footer">
+                <div>480-378-3270</div>
+                <div>SALES@KERSONDYNAMICS.COM</div>
+                <div>9917 E.BELL RD. STE.120 / SCOTTSDALE, AZ/ 85260</div>
+            </div>
+        )
+    }
+
+}
