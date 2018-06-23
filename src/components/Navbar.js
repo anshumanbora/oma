02260a94f0a673f59ff881cdb53f9b39bec7b9f7
Anshuman Bora <anshumanbora04@gmail.com>
2018-06-23 13:40:01 -0700

    navbar css updated + underlining added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,27 +5,29 @@ export default class Navbar extends Component{
     render(){
         return(
             <div className="navbar">
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                {/*setting propperties of the navigatin bar*/}
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                    {/*button for navbar when screen is small*/}
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-
+                    {/*contents of the navbar*/}
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav mr-auto">
                             <li className="nav-item">
-                                <a className="nav-link" href="#">HOME</a>
+                                <a className="nav-link underline" href="#">HOME</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">ROYALTY ADVOCATE</a>
+                                <a className="nav-link underline" href="#">ROYALTY ADVOCATE</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">ABOUT</a>
+                                <a className="nav-link underline" href="#">ABOUT</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">CAREERS</a>
+                                <a className="nav-link underline" href="#">CAREERS</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">CONTACT US</a>
+                                <a className="nav-link underline" href="#">CONTACT US</a>
                             </li>
                         </ul>
 
